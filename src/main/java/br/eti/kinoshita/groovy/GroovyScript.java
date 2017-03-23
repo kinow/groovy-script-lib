5c3958c062a82a3af3ff23570a7cdfd7a86147be
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-03-23 23:09:10 +1300

    Adding secure scripts and more dependencies

--- src/main/java/br/eti/kinoshita/groovy/GroovyScript.java
+++ src/main/java/br/eti/kinoshita/groovy/GroovyScript.java
@@ -3,7 +3,30 @@ package br.eti.kinoshita.groovy;
 import java.util.Collections;
 import java.util.Map;
 
+import javax.annotation.CheckForNull;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript;
+
 public class GroovyScript<T> extends AbstractScript<T> {
+
+    private final SecureGroovyScript script;
+    private final SecureGroovyScript fallbackScript;
+
+    public GroovyScript(String script) {
+        this(script, null);
+    }
+    
+    public GroovyScript(String script, @CheckForNull String fallback) {
+        this(new SecureGroovyScript(script, true, null),
+                StringUtils.isBlank(fallback) ?
+                        null : new SecureGroovyScript(fallback, true, null));
+    }
+
+    public GroovyScript(SecureGroovyScript secureGroovyScript, SecureGroovyScript fallbackSecureGroovyScript) {
+        this.script = secureGroovyScript;
+        this.fallbackScript = fallbackSecureGroovyScript;
+    }
 
     public T eval() {
         return eval(Collections.emptyMap());
