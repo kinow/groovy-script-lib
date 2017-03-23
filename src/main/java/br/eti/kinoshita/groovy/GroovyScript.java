de3aac0253ecae6b2c097fb41fd306981efd6e48
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-03-23 22:35:54 +1300

    Initial implementation

--- src/main/java/br/eti/kinoshita/groovy/GroovyScript.java
+++ src/main/java/br/eti/kinoshita/groovy/GroovyScript.java
@@ -1,5 +1,16 @@
 package br.eti.kinoshita.groovy;
 
-public class GroovyScript {
+import java.util.Collections;
+import java.util.Map;
+
+public class GroovyScript<T> extends AbstractScript<T> {
+
+    public T eval() {
+        return eval(Collections.emptyMap());
+    }
+
+    public <K, V> T eval(Map<K, V> parameters) {
+        return null;
+    }
 
 }
